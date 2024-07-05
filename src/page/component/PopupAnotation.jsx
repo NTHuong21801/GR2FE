d2396554dc6e9e2ddb8b750dfec6906c6612642c
NTHuong21801 <103840627+NTHuong21801@users.noreply.github.com>
2024-07-05 11:41:17 +0700

    popup genfile

--- src/page/component/PopupAnotation.jsx
+++ src/page/component/PopupAnotation.jsx
@@ -13,7 +13,7 @@ export default function PopupAnotation({onClose}){
                             <li>Khi nhận được file danh sách sinh viên từ QLĐT, giảng viên cần thực hiện chức năng import danh sách
                                 sinh viên vào hệ thống để kiểm soát sinh viên cũng như bắt đầu tạo các phiếu giao.
                             </li>
-                            <li>Trường hợp các trường thông tin liên quan đến sinh viên thì giảng viên chỉ việc export lại từ QLĐT, 
+                            <li>Trường hợp các trường thông tin liên quan đến sinh viên thay đổi thì giảng viên chỉ việc export lại từ QLĐT, 
                                 và import lại vào Website này thì hệ thống sẽ ghi đè theo MSSV</li>
                             <li>
                                 Nếu giảng viên upload các phiếu từ giao diện thì các thông tin trong phiếu sẽ được ghi đè vào danh sách này
